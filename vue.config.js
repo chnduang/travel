3a260ee5df59b2ac4988647a3be78859404a8dfb
ZQD <zhouqd1997@163.com>
2019-08-17 13:56:35 +0800

    init-project

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+// vue.config.js
+module.exports = {
+    configureWebpack: {
+        resolve: {
+            alias: {
+              'styles':'@/assets/styles'
+            }
+          },
+    }
+  }
